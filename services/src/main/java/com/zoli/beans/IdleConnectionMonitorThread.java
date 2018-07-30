6dd0f573a83cd076acf8c3148c25400200da7241
Zoltan Kovacs <z.kovacs@leonardo.com.au>
2018-07-30 17:37:32 +1000

    keepalive true

--- services/src/main/java/com/zoli/beans/IdleConnectionMonitorThread.java
+++ services/src/main/java/com/zoli/beans/IdleConnectionMonitorThread.java
@@ -29,7 +29,7 @@ public class IdleConnectionMonitorThread implements Runnable {
                 while (!shutdown) {
                     synchronized (this) {
                         System.out.println("Hello from Monitor thread");
-                        wait(1000 * 60 );
+                        wait(1000 );
                         System.out.println("Monitor thread closing expired connections");
                         // Close expired connections
                         connMgr.closeExpiredConnections();
